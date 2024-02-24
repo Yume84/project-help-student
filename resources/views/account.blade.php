efcc4b8c8ea396e96fb9b217d58865b2f23054c6
Mylane <mylaaane@gmail.com>
2024-02-25 00:34:14 +0100

    Account

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,18 +1,24 @@
 <x-admin-layout>
 
 <!--Couverture-->
-<div class=" bg-dark-blue w-full h-60">
+<div class="bg-dark-blue w-full h-60">
     <div class="flex items-center max-w-5xl mx-auto p-4 sm:p-6 lg:p-8">
 
-        <!--Photo de profil-->
-        <img src="{{ asset('/images/exemple.jpg') }}" alt="avatar" class="block w-40 h-40 object-cover rounded border-blue ">
+        <!-- Photo de profil -->
+        <img src="{{ asset('/images/exemple.jpg') }}" alt="avatar" class="block w-40 h-40 object-cover rounded border-blue">
 
-        <!--Contenu à droite de la photo-->
-        <div class="ml-10 flex flex-col">
-            <div x-data="{ name: '{{ auth()->user()->name }}' }" x-text="name" x-on:profile-updated.window="name = $event.detail.name" class="text-blue"></div>
-            <span class="mt-2 text-light-blue">Le statut sera ici</span>
-            <x-post-button class="bg-blue hover:bg-yellow hover:text-dark-blue mt-4">{{ __('Edit Profile') }}</x-post-button>
-        </div>
+        <!-- Contenu à droite de la photo -->
+        <div class="ml-10 flex flex-col sm:flex-row sm:items-center flex-grow">
+            <div>
+                <div x-data="{ name: '{{ auth()->user()->name }}' }" x-text="name" x-on:profile-updated.window="name = $event.detail.name" class="text-blue"></div>
+                <span class="mt-2 text-light-blue">Le statut sera ici</span>
+            </div>
+
+            <!-- Bouton "Modifier le profil" -->
+            <div class="sm:ml-auto">
+                <a href="{{ route('profile') }}" class="bg-blue hover:bg-yellow hover:text-dark-blue inline-flex items-center mt-4 px-4 py-2 bg-dark-blue border border-transparent rounded-md font-semibold text-xs text-light-blue uppercase tracking-widest hover:bg-blue transition ease-in-out duration-150">{{ __('Edit Profile') }}</a>            </div>
+            </div>
+
     </div>
 </div>
 
