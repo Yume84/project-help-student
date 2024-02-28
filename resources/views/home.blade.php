18b87e1597352059ace60db6f9a2c14b98f38469
Mylane <mylaaane@gmail.com>
2024-02-28 16:44:56 +0100

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,93 +21,95 @@
 
         <!-- Présentation -->
         <div class="text-center mb-14">
-            <h2 class="inline-block font-bold uppercase text-light-blue text-2xl bg-dark-blue border-dark-blue p-3 mb-4 ">Studdle, c'est quoi ?</h2>
-            <p class="text-lg text-left mt-4 mb-8">Studdle est une plateforme dédiée aux jeunes universitaires étrangers venus étudier en France afin de les aider dans leurs démarches administratives. Mais ce n'est pas tout ! Si tu es un grand philanthrope, il est également possible que tu proposes ton aide. 😉</p>
+            <h2 class="inline-block font-bold uppercase text-light-blue text-2xl bg-dark-blue border-dark-blue p-2 mb-4 rounded-lg">Studdle, c'est quoi ?</h2>
+            <p class="text-lg text-left mt-4 mb-8">Studdle est une plateforme dédiée aux jeunes universitaires étrangers venus étudier en Ile-de-France afin de les aider dans leurs démarches administratives. Mais ce n'est pas tout ! Si tu es un grand philanthrope, il est également possible que tu proposes ton aide. 😉</p>
         </div>
 
         <!-- Fonctionnement -->
-        <div class="text-center mb-14">
-            <h2 class="inline-block font-bold uppercase text-light-blue text-2xl bg-dark-blue border-dark-blue p-3 mb-4 ">Comment ça fonctionne ?</h2>
+        <div class="text-center mb-8">
+            <h2 class="inline-block font-bold uppercase text-light-blue text-2xl bg-dark-blue border-dark-blue p-2 mb-4 rounded-lg">Comment ça fonctionne ?</h2>
 
             <!-- Sans assistance -->
             <div>
-                <h2 class="inline-block font-bold uppercase text-light-blue text-xl bg-blue border-blue p-2 mb-4 ">Sans assistance</h2>
+                <h2 class="inline-block font-bold uppercase text-light-blue text-xl bg-blue border-blue p-1 mb-4 mt-2 rounded-lg">Sans assistance</h2>
                 <p class="text-lg text-left mt-4 mb-8">Un récapitulatif des aides administratives principales sous forme de fiches individuelles sont accessibles sans inscription. Ce récapitulatif permet de centraliser les informations d'habitude présentes sur plusieurs sites afin de permettre une navigation simplifiée en toute autonomie.</p>
             </div>
 
             <!-- Avec assistance -->
             <div>
-                <h2 class="inline-block font-bold uppercase text-light-blue text-xl bg-blue border-blue p-2 mb-4 ">Avec assistance</h2>
+                <h2 class="inline-block font-bold uppercase text-light-blue text-xl bg-blue border-blue p-1 mb-4 rounded-lg">Avec assistance</h2>
                 <p class="text-lg text-left mt-4 mb-8">'autre un espace réservé uniquement aux étudiants des Campus de l'Île-de-France pour créer de la proximité et t'aider dans tes démarches.</p>
+            </div>
+
+        </div>
+
+        <!-- Etapes de fonctionnement -->
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-8 mb-14">
+
+            <!-- Etape 1 -->
+            <div class="p-6 bg-light-blue rounded-lg shadow-2xl shadow-gray-500/20">
+                <div>
+                    <div class="h-16 w-16 bg-white flex items-center justify-center rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="block text-blue w-10 h-10">
+                            <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+
+                    <h2 class="mt-6 text-xl font-semibold text-blue">Etape 1 : Création du compte</h2>
+
+                    <p class="mt-4 text-dark-blue text-sm leading-relaxed">
+                        Tout le monde peut se créer un compte : les étudiants qui font parti d'un des campus universitaires situés en Ile-de-France et toutes les personnes souhaitant les aider dans leur processus administratif.
+                    </p>
+                </div>
+            </div>
+
+            <!-- Etape 2 -->
+            <div class="p-6 bg-light-blue rounded-lg shadow-2xl shadow-gray-500/20">
+                <div>
+                    <div class="h-16 w-16 bg-white flex items-center justify-center rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="block text-blue w-10 h-10">
+                            <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+
+                    <h2 class="mt-6 text-xl font-semibold text-blue">Etape 2 : Publication d'annonce</h2>
+
+                    <p class="mt-4 text-dark-blue text-sm leading-relaxed">
+                        Après ton inscription, que tu demandes de l'aide ou que tu proposes de l'aide, une annonce sera automatiquement publiée pour informer les autres utilisateurs de tes intentions. Il te sera possible de publier d'autres annonces quand tu le voudras.
+                    </p>
+                </div>
+            </div>
+
+            <!-- Etape 3 -->
+            <div class="p-6 bg-light-blue rounded-lg shadow-2xl shadow-gray-500/20">
+                <div>
+                    <div class="h-16 w-16 bg-white flex items-center justify-center rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="block text-blue w-10 h-10">
+                            <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+
+                    <h2 class="mt-6 text-xl font-semibold text-blue">Etape 3 : Mise en relation</h2>
+
+                    <p class="mt-4 text-dark-blue text-sm leading-relaxed">
+                        L'application se base sur le bon vouloir de chacun. Des volontaires répondront à ton annonce dans les meilleurs délais grâce à un système de messagerie privée !
+                    </p>
+                </div>
             </div>
 
         </div>
 
         <!-- Cible -->
         <div class="text-center mb-14">
-            <h2 class="inline-block font-bold uppercase text-light-blue text-2xl bg-dark-blue border-dark-blue p-3 mb-4 ">Destiné à qui ?</h2>
+            <h2 class="inline-block font-bold uppercase text-light-blue text-2xl bg-dark-blue border-dark-blue p-2 mb-4 rounded-lg">Studdle, destiné à qui ?</h2>
             <p class="text-lg text-left mt-4 mb-8">Tu t'es lancé dans l'aventure en faisant tes études en France et tu fais partie de l'une des universités d'Ile-de-France ?</p>
             <p class="text-lg text-left">Tu ne maîtrises pas bien la langue française et tu te perds dans tes démarches administratives ?</p>
         </div>
 
-        <!-- Etapes de fonctionnement -->
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-8">
-            <div class="p-6 bg-light-blue rounded-lg shadow-2xl shadow-gray-500/20">
-                <div>
-                    <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-red-500">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
-                        </svg>
-                    </div>
-
-                    <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Etape 1 : Création du compte</h2>
-
-                    <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                        Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
-                    </p>
-                </div>
-            </div>
-
-            <a href="https://laracasts.com" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                <div>
-                    <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-red-500">
-                            <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-                        </svg>
-                    </div>
-
-                    <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laracasts</h2>
-
-                    <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                        Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-                    </p>
-                </div>
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                </svg>
-            </a>
-
-            <a href="https://laracasts.com" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                <div>
-                    <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-red-500">
-                            <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-                        </svg>
-                    </div>
-
-                    <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laracasts</h2>
-
-                    <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                        Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-                    </p>
-                </div>
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                </svg>
-            </a>
-
+        <!-- En savoir plus -->
+        <div class="text-center mb-8">
+            <h2 class="inline-block font-bold uppercase text-light-blue text-2xl bg-dark-blue border-dark-blue p-2 mb-4 rounded-lg">Récapitulatif des aides</h2>
+            <p class="text-lg text-left mt-4 mb-8">Retrouve ici les aides administratives principales en fonction de leur thématique.</p>
         </div>
 
     </div>
