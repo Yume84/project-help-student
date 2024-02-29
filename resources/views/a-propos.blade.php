c0fb60e7e9ab31c464f32babc596d6cecf4c02f1
Mylane <mylaaane@gmail.com>
2024-02-29 13:17:39 +0100

    home

---
+++ resources/views/a-propos.blade.php
@@ -0,0 +1,10 @@
+<x-home-layout>
+
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8" >
+
+       salut
+
+    </div>
+
+</x-home-layout>
+
--- resources/views/livewire/welcome/studdle.blade.php
+++ resources/views/livewire/welcome/studdle.blade.php
@@ -1,11 +1,11 @@
 <div class="flex flex-col space-y-4">
     @auth
-        <a href="{{ url('/dashboard') }}" class="px-4 py-2 bg-blue border border-transparent rounded-md font-semibold text-xs text-light-blue uppercase tracking-widest hover:bg-blue transition ease-in-out duration-150" wire:navigate>Dashboard</a>
+        <a href="{{ url('/dashboard') }}" class="px-6 py-3 bg-blue border border-transparent rounded-md font-extrabold text-light-blue uppercase tracking-widest hover:bg-blue transition ease-in-out duration-150" wire:navigate>Dashboard</a>
     @else
-        <a href="{{ route('login') }}" class="px-4 py-2 bg-blue border border-transparent rounded-md font-semibold text-xs text-light-blue uppercase tracking-widest hover:bg-blue transition ease-in-out duration-150" wire:navigate>J'ai déjà un compte</a>
+        <a href="{{ route('login') }}" class="px-6 py-3 bg-blue border border-transparent rounded-md font-extrabold text-light-blue uppercase tracking-widest hover:bg-blue transition ease-in-out duration-150" wire:navigate>J'ai déjà un compte</a>
 
         @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="px-4 py-2 bg-blue border border-transparent rounded-md font-semibold text-xs text-light-blue uppercase tracking-widest hover:bg-blue transition ease-in-out duration-150" wire:navigate>C'est parti !</a>
+            <a href="{{ route('register') }}" class="px-6 py-3 bg-blue border border-transparent rounded-md font-extrabold text-light-blue uppercase tracking-widest hover:bg-blue transition ease-in-out duration-150" wire:navigate>C'est parti !</a>
         @endif
     @endauth
 </div>
