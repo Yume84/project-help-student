6ae36e756e6f376145c1ec6159a451943a1fd49f
Mylane <mylaaane@gmail.com>
2024-03-15 23:04:26 +0100

    ajustement tunnel

--- resources/views/components/radio-input.blade.php
+++ resources/views/components/radio-input.blade.php
@@ -9,7 +9,7 @@
     x-data="{ isActive: false }"
     @click="isActive = !isActive"
     :class="{ 'bg-yellow': isActive, 'bg-light-blue': !isActive }"
-    class="bg-light-blue h-24 ombre-blue rounded-lg cursor-pointer flex items-center justify-center text-dark-blue text-lg mb-6" 
+    class="bg-light-blue h-24 ombre-blue rounded-lg cursor-pointer flex items-center justify-center text-dark-blue text-lg mb-4" 
     for="{{ $attributes['id'] }}"
 >
     {{ $slot }}
--- resources/views/layouts/step.blade.php
+++ resources/views/layouts/step.blade.php
@@ -15,12 +15,23 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased bg-dark-blue">
-        <div class="min-h-screen flex flex-col justify-center items-center pb-10 mx-6 rounded"> <!--min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 mx-4 bg-gray-100 dark:bg-gray-900-->
+        <div class="min-h-screen flex-col justify-center items-center mx-6 rounded"> <!--min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 mx-4 bg-gray-100 dark:bg-gray-900-->
 
-            <div class="w-full max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 overflow-hidden">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 pt-12 overflow-hidden">
+                <a href="{{ route('welcome') }}">
+                    <x-register-button type="button" class="px-2.5 py-2.5">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="block text-light-blue w-8 h-8">
+                            <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                            <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                        </svg>
+                    </x-register-button>
+                </a>
+            </div>
+
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 overflow-hidden">
                 @yield('content')
             </div>
-            
+
         </div>
     </body>
 </html>
