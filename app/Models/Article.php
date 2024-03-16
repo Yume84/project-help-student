8c438727b18a00f8d0680ea74e5d0f9d22badc39
Pierre-Adrien Carton <pierre-adrien@qweekle.com>
2024-03-16 07:10:03 +0100

    add Localized scope

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,12 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\LocalizedScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new LocalizedScope());
+    }
 
     public function tags(){
         return $this->belongsToMany(Tag::class);
