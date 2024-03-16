d33ddb7e626bb66aa6edc03dc55d0f07ff992f16
Pierre-Adrien Carton <pierre-adrien@qweekle.com>
2024-03-16 06:07:46 +0100

    made article seeder more dynamic

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Str;
 class ArticleSeeder extends Seeder
 {
     /**
@@ -34,124 +34,98 @@ class ArticleSeeder extends Seeder
         // Todo : variable à remplacer par le vrai contenu HTML de chaque article.
         $content_placeholder    = '<h1>TODO : insérer le contenu dans le seeder</h1>';
         $now                    = now()->toDatetimeString();
+
+        $articles           =   [
+                                    [
+                                        "title"     => "Carte vitale",
+                                        "content"   => $content_placeholder,
+                                        "tags"      => [1]
+                                    ],
+                                    [
+                                        "title"     => "Ameli",
+                                        "content"   => $content_placeholder,
+                                        "tags"      => [1]
+                                    ],
+                                    [
+                                        "title"     => "CAF",
+                                        "content"   => $content_placeholder,
+                                        "tags"      => [2]
+                                    ],
+                                    [
+                                        "title"     => "Inscription universitaire",
+                                        "content"   => $content_placeholder,
+                                        "tags"      => [3]
+                                    ],
+                                    [
+                                        "title"     => "Demande de logement",
+                                        "content"   => $content_placeholder,
+                                        "tags"      => [4]
+                                    ],
+                                    [
+                                        "title"     => "Assurance habitation",
+                                        "content"   => $content_placeholder,
+                                        "tags"      => [4]
+                                    ],
+                                    [
+                                        "title"     => "Ouvrir un compte bancaire",
+                                        "content"   => $content_placeholder,
+                                        "tags"      => [5]
+                                    ],
+                                    [
+                                        "title"     => "Demande de visa avant son arrivée",
+                                        "content"   => $content_placeholder,
+                                        "tags"      => [6]
+                                    ],
+                                    [
+                                        "title"     => "Demande de renouvellement de visa",
+                                        "content"   => $content_placeholder,
+                                        "tags"      => [6]
+                                    ],
+                                    [
+                                        "title"     => "Acte de naissance",
+                                        "content"   => $content_placeholder,
+                                        "tags"      => [6]
+                                    ],
+                                ];
+
+        // on initalise les deux tableaux à insérer en DB
+        $insert_articles    = [];
+        $insert_article_tag = [];
+        
+        foreach(\App\Models\Language::all() as $language) // pour chaque langue
+        {
+            foreach($articles as $article_index => $article) // on insert une version de chaque article
+            {
+                // on insert une version de chaque article
+                $insert_articles[] =    [
+                                            "title"     => __($article['title']),
+                                            "content"   => $article['content'], // placeholder pour l'instant le contenu n'est pas traduit
+                                            "lang_id"   => $language->id,
+                                            "slug"      => Str::slug(__($article['title'])),
+                                            "created_at"=> $now,
+                                            "updated_at"=> $now
+                                        ];
+                // Pour chaque tag associés à l'article
+                foreach($article['tags'] as $tag)
+                {
+                    // on insert un article_tag correspondant
+                    $insert_article_tag[] = [
+                        "article_id" => $article_index + 1,
+                        "tag_id"     => $tag
+                    ];
+                }
+            }
+        }
+
+        // nos deux tableaux sont prêts
+        // on les insère en DB
+
         DB::table('articles')->insert(
-            [
-                [
-                    "title"     => "Cart vitale",
-                    "content"   => $content_placeholder,
-                    "lang_id"   => 1,
-                    "created_at"=> $now,
-                    "updated_at"=> $now
-                ],
-                [
-                    "title"     => "Ameli",
-                    "content"   => $content_placeholder,
-                    "lang_id"   => 1,
-                    "created_at"=> $now,
-                    "updated_at"=> $now
-                ],
-                [
-                    "title"     => "CAF",
-                    "content"   => $content_placeholder,
-                    "lang_id"   => 1,
-                    "created_at"=> $now,
-                    "updated_at"=> $now
-                ],
-                [
-                    "title"     => "Inscription universitaire",
-                    "content"   => $content_placeholder,
-                    "lang_id"   => 1,
-                    "created_at"=> $now,
-                    "updated_at"=> $now
-                ],
-                [
-                    "title"     => "Demande de logement",
-                    "content"   => $content_placeholder,
-                    "lang_id"   => 1,
-                    "created_at"=> $now,
-                    "updated_at"=> $now
-                ],
-                [
-                    "title"     => "Assurance habitation",
-                    "content"   => $content_placeholder,
-                    "lang_id"   => 1,
-                    "created_at"=> $now,
-                    "updated_at"=> $now
-                ],
-                [
-                    "title"     => "Ouvrir un compte bancaire",
-                    "content"   => $content_placeholder,
-                    "lang_id"   => 1,
-                    "created_at"=> $now,
-                    "updated_at"=> $now
-                ],
-                [
-                    "title"     => "Demande de visa avant son arrivée",
-                    "content"   => $content_placeholder,
-                    "lang_id"   => 1,
-                    "created_at"=> $now,
-                    "updated_at"=> $now
-                ],
-                [
-                    "title"     => "Demande de renouvellement de visa",
-                    "content"   => $content_placeholder,
-                    "lang_id"   => 1,
-                    "created_at"=> $now,
-                    "updated_at"=> $now
-                ],
-                [
-                    "title"     => "Acte de naissance",
-                    "content"   => $content_placeholder,
-                    "lang_id"   => 1,
-                    "created_at"=> $now,
-                    "updated_at"=> $now
-                ],
-            ]
+            $insert_articles
         );
-        // on créer les entrées dans la table pivot pour assigner des tags à des articles
+        
         DB::table('article_tag')->insert(
-            [
-                [
-                    "article_id"    => 1,
-                    "tag_id"        => 1,
-                ],
-                [
-                    "article_id"    => 2,
-                    "tag_id"        => 1,
-                ],
-                [
-                    "article_id"    => 3,
-                    "tag_id"        => 2,
-                ],
-                [
-                    "article_id"    => 4,
-                    "tag_id"        => 3,
-                ],
-                [
-                    "article_id"    => 5,
-                    "tag_id"        => 4,
-                ],
-                [
-                    "article_id"    => 6,
-                    "tag_id"        => 4,
-                ],
-                [
-                    "article_id"    => 7,
-                    "tag_id"        => 5,
-                ],
-                [
-                    "article_id"    => 8,
-                    "tag_id"        => 6,
-                ],
-                [
-                    "article_id"    => 9,
-                    "tag_id"        => 6,
-                ],
-                [
-                    "article_id"    => 10,
-                    "tag_id"        => 6,
-                ],
-            ]
+            $insert_article_tag
         );
     }
 }
