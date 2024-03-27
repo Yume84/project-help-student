a45b16840b8ab45167a7507d054be5a8c2b34c86
Aurelie <asatta0310@gmail.com>
2024-03-27 03:36:50 +0100

    test

--- resources/views/livewire/messages-conversation.blade.php
+++ resources/views/livewire/messages-conversation.blade.php
@@ -13,27 +13,5 @@
             <button type="submit" class="mt-4 inline-block px-4 py-2 bg-blue-500 text-white rounded-md">{{ __('Send') }}</button>
         </form>
 
-        <!-- Affichage des messages de la conversation -->
-<div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
-            @foreach ($messages as $message)
-                <div class="p-4">
-                    <div class="flex items-center">
-                        <div class="flex-shrink-0 mr-3">
-                            <!-- Afficher ici l'avatar de l'utilisateur qui a envoyé le message -->
-                            <img class="w-8 h-8 rounded-full" src="{{ $message->user->avatar_url }}" alt="{{ $message->user->name }}">
-                        </div>
-                        <div>
-                            <div class="font-semibold">{{ $message->user->name }}</div>
-                            <div class="text-gray-600">{{ $message->created_at->format('d/m/Y H:i') }}</div>
-                        </div>
-                    </div>
-                    <div class="mt-2">
-                        <!-- Afficher ici le contenu du message -->
-                        <p>{{ $message->content }}</p>
-                    </div>
-                </div>
-            @endforeach
-</div>
-
     </div>
 </x-admin-layout>
